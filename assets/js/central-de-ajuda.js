a5df53611fe78d9df63aaca351dccc8e897f54b8
LaryssaFerrari <lalaferrariok@gmail.com>
2023-10-19 20:07:43 -0300

    Modificação central de ajuda

--- assets/js/central-de-ajuda.js
+++ assets/js/central-de-ajuda.js
@@ -1,8 +1,31 @@
-const accordion = document.getElementsByClassName
-    ('contentBx');
+const accordion_item = document.querySelectorAll(".accordion_item");
 
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('active')
-    })
+accordion_item.forEach((item) => {
+    const accordion_header_item = item.querySelector(".accordion_header");
+
+    accordion_header_item.addEventListener("click", () => {
+        const accordion_content_item = item.querySelector(".accordion_content");
+
+        const item_actived = document.querySelector(".active");
+
+        VerifyActive(item, accordion_content_item, item_actived);
+    });
+});
+
+function VerifyActive(item, content, content_actived) {
+    const icon_item = item.querySelector(".icon");
+    const icon_item_active = document.querySelectorAll(".icon");
+
+    icon_item_active.forEach((item) => (item.innerHTML = "+"));
+
+    if (content_actived) {
+        content_actived.style.height = 0;
+        content_actived.classList.remove("active");
+    }
+
+    if (content !== content_actived) {
+        icon_item.innerHTML = "-";
+        content.classList.add("active");
+        content.style.height = content.scrollHeight + 10 + "px";
+    }
 }
